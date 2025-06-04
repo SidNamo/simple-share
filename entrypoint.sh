b0532b6bdb8031c9a25a163bf6874c6d2799782a
SidNamo <43488826+SidNamo@users.noreply.github.com>
2025-06-04 21:33:53 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ echo "OWNER_DOMAIN=${OWNER_DOMAIN}" >> /app/.env
 /usr/sbin/cron
 
 # 2. FastAPI(Uvicorn) 서버 실행 (포그라운드)
-uvicorn main:app --host 0.0.0.0 --port 80
+/app/.venv/bin/uvicorn main:app --host 0.0.0.0 --port 80
